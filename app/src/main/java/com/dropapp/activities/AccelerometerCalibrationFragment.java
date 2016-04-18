79c3fd91243f47582060ca19e6cee435059feda2
Benjamin Chevoor <bchevoor@gmail.com>
2016-04-18 19:16:40 -0400

    Bettering drop calibration

--- app/src/main/java/com/dropapp/activities/AccelerometerCalibrationFragment.java
+++ app/src/main/java/com/dropapp/activities/AccelerometerCalibrationFragment.java
@@ -1,19 +1,23 @@
 package com.dropapp.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dropapp.R;
 import com.dropapp.util.Settings;
@@ -39,6 +43,9 @@ public class AccelerometerCalibrationFragment extends Fragment {
 
     private TextView thresholdTextView;
     private ProgressBar progressBar;
+    private boolean isCalibrating = false;
+    private int runningAttempts = 0;
+    private int runningAverage = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,31 +65,87 @@ public class AccelerometerCalibrationFragment extends Fragment {
         startCalibrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startCalibration();
+                runningAverage = 0;
+                runningAttempts = 0;
+                startCalibrationAttempt();
             }
         });
 
         return parentView;
     }
 
-    private void startCalibration() {
-        this.thresholdTextView.setText("");
-        this.progressBar.setVisibility(View.VISIBLE);
+    private void startCalibrationAttempt() {
+        if (!isCalibrating) {
+            this.isCalibrating = true;
+            this.runningAttempts++;
 
-        new Calibration(this.getContext());
+            this.thresholdTextView.setText("");
+
+            final Toast t = Toast.makeText(getContext(), "...", Toast.LENGTH_SHORT);
+
+            final Handler h = new Handler();
+            Runnable toastRunnable = new Runnable() {
+                private int i = 3;
+
+                @Override
+                public void run() {
+                    if (i > 0) {
+                        t.setText(i-- + "...");
+                        t.show();
+                        h.postDelayed(this, 1000);
+                    } else {
+                        t.setText("Drop!");
+                        t.show();
+                    }
+                }
+            };
+
+            h.post(toastRunnable);
+
+            Runnable calibrationRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    progressBar.setVisibility(View.VISIBLE);
+                    new Calibration(getContext());
+                }
+            };
+            h.postDelayed(calibrationRunnable, 3000);
+        }
     }
 
-    private void stopCalibration(double newThreshold) {
-        this.thresholdTextView.setText(String.format("New threshold: %.4f", newThreshold));
-        this.progressBar.setVisibility(View.INVISIBLE);
-
-        Settings.setThreshold(this.getContext(), newThreshold);
+    private void stopCalibrationAttempt(double newThreshold) {
+        this.isCalibrating = false;
+        int threshold = (int) Math.round(newThreshold);
+        this.runningAverage += threshold;
 
         Vibrator v = (Vibrator) this.getContext().getSystemService(Context.VIBRATOR_SERVICE);
         // Vibrate for 500 milliseconds
         v.vibrate(500);
 
-        ((SetupActivity) this.getActivity()).calibrationFinished();
+        Toast.makeText(getContext(), String.format("Threshold: %d", threshold), Toast.LENGTH_SHORT).show();
+
+        if (runningAttempts < 3) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setMessage("Drop again!");
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialogInterface) {
+                    startCalibrationAttempt();
+                }
+            });
+            builder.setPositiveButton("Okay!", null);
+            builder.create().show();
+
+        } else {
+            int average = this.runningAverage / 3;
+
+            this.thresholdTextView.setText(String.format("New threshold: %d", average));
+            this.progressBar.setVisibility(View.INVISIBLE);
+
+            Settings.setThreshold(this.getContext(), newThreshold);
+
+            ((SetupActivity) this.getActivity()).calibrationFinished();
+        }
     }
 
     private class Calibration implements SensorEventListener {
@@ -118,7 +181,7 @@ public class AccelerometerCalibrationFragment extends Fragment {
                     SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
                     sensorManager.unregisterListener(this);
 
-                    stopCalibration(this.highestVector);
+                    stopCalibrationAttempt(this.highestVector);
                 }
             }
         }
